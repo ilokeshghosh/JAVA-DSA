2ab4aa3f38838b7b1f5eb7dbd9816e31814df2f2
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-07 12:07:54 +0530

    Create Q7.java

---
+++ JAVA-DSA/src/Practice/Array/Test/Q7.java
@@ -0,0 +1,16 @@
+package Practice.Array.Test;
+/*
+
+Number of Good Pairs
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+ */
+public class Q7 {
+    public static void main(String[] args){
+        int[] nums={1,2,3,1,1,3};
+        
+    }
+}

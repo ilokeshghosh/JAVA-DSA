7d1b2e04fe67398acdf15deb4b1b8f9b9350aa74
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-07 15:14:00 +0530

    .

--- JAVA-DSA/src/Practice/Array/Test/Q7.java
+++ JAVA-DSA/src/Practice/Array/Test/Q7.java
@@ -11,6 +11,17 @@ Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 public class Q7 {
     public static void main(String[] args){
         int[] nums={1,2,3,1,1,3};
+        int count=0;
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j] && i<j){
+                    count++;
+                }
+            }
+
+        }
+        System.out.println(count);
 
     }
 }

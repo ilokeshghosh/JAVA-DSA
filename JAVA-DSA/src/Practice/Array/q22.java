3d53f4d2aa169db584c04ec3dcf3b160fcf6ae93
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-24 09:42:50 +0530

    .

--- JAVA-DSA/src/Practice/Array/q22.java
+++ JAVA-DSA/src/Practice/Array/q22.java
@@ -1,4 +1,8 @@
 package Practice.Array;
+import java.lang.*;
+import java.util.Arrays;
+import java.util.Random;
+
 /*
 
 Find N Unique Integers Sum up to Zero
@@ -11,6 +15,29 @@ Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4]
 public class q22 {
     public static void main(String[] args) {
 //        int n = 3;
-        int n=5;
+        int n = 9;
+//        int n = 5;
+        System.out.println(Arrays.toString(sumZero(n)));
+
+    }
+
+    static int[] sumZero(int n) {
+        int[] answer = new int[n];
+        int index = 0;
+
+        if (n % 2 == 1){
+            answer[index++] = 0;
+        }
+
+
+        n /= 2;
+
+        for (int i = 1; i <= n; i++) {
+            answer[index++] = i;
+            answer[index++] = -i;
+        }
+
+        return answer;
+
     }
 }
--- JAVA-DSA/src/Tutorial/ConditionalLoops/casecheck.java
+++ JAVA-DSA/src/Tutorial/ConditionalLoops/casecheck.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class casecheck {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
+        System.out.print("Enter the Character : ");
         char l =in.next().trim().charAt(0);
 
         if(l>='a' && l<='z'){

0b322db63a12251a7d9b1298ea3b1e045326bec2
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-26 22:22:59 +0530

    .

--- JAVA-DSA/src/Practice/Array/q23.java
+++ JAVA-DSA/src/Practice/Array/q23.java
@@ -1,6 +1,7 @@
 package Practice.Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -23,26 +24,65 @@ public class q23 {
 //        };
 
 //        test case-2
-        int[][] matrix={
-                {1,10,4,2},
-                {9,3,8,7},
-                {15,16,17,12}
+//        int[][] matrix={
+//                {1,10,4,2},
+//                {9,3,8,7},
+//                {15,16,17,12}
+//        };
+
+
+        //test case-3
+//        int[][] matrix={
+//                {7,8},
+//                {1,2}
+//
+//        };
+
+
+        //test case -4
+        int[][] matrix ={
+                {3,6},
+                {7,1},
+                {5,2},
+                {4,8}
+
         };
+
         System.out.println(luckyNumbers(matrix));
 
     }
     static List<Integer> luckyNumbers (int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(i== matrix.length-1 && matrix[i][j]==matrix[i][0]){
-//                    System.out.println(matrix[i][j]);
-                    ans.add(matrix[i][j]);
-                }
 
+        for(int row = 0;row<matrix.length;row++){
+            for(int col=0;col<matrix[row].length;col++){
+                if(isMinInRow(matrix,matrix[row][col],row) && isMaxIncol(matrix,matrix[row][col],col)){
+                    ans.add(matrix[row][col]);
+                }
             }
         }
+
         return ans;
 
+    }
+
+    static boolean isMinInRow(int[][] row,int number ,int rowNum){
+        int min=0;
+        boolean ans = false;
+        for(int col=0;col<row[rowNum].length;col++){
+            if(row[rowNum][col]<number){
+                return false;
+            }
+        }
+        return true;
+    }
+    static boolean isMaxIncol(int[][] col,int number,int colNum){
+        boolean ans = false;
+        for(int i=0;i<col.length;i++){
+            if(number<col[i][colNum]){
+                return false;
+            }
+        }
+        return true;
     }
 }

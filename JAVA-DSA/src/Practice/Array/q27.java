4883b58264123f8e8c6ea2e41ce6d1b3a38b7784
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-05 21:24:37 +0530

    Create q27.java

---
+++ JAVA-DSA/src/Practice/Array/q27.java
@@ -0,0 +1,37 @@
+package Practice.Array;
+/*
+
+Remove Duplicates from Sorted Array
+
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+ */
+public class q27 {
+    public static void main(String[] args) {
+
+
+//        test case-1
+//        int[] nums ={1, 1, 2};
+//        output : 2
+
+
+        //test case-2
+        int[] nums= {0,0,1,1,1,2,2,3,3,4};
+//        output : 5
+
+    }
+
+    static int removeDuplicates(int[] nums) {
+
+        return -1;
+    }
+}
